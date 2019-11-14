8727b56de30eb720c7ac1a83726fe282034a45e4
Aluno IFAL <aluno.ifal@LABS.RIOLARGO>
2019-11-14 11:59:16 -0300

    alteracao do codigo

--- cadastroalunorayane/src/main/java/ifal/edu/br/cadastroalunorayane/Controlador.java
+++ cadastroalunorayane/src/main/java/ifal/edu/br/cadastroalunorayane/Controlador.java
@@ -15,4 +15,16 @@ public class Controlador{
     public ModelAndView index(){
         return new ModelAndView("index.html");
     }
+
+    @RequestMapping("/dados")
+    public ModelAndView dados(String nome, String email, String senha, String modulo, String preferencia){
+        ModelAndView retorno = new ModelAndView("tabela.html");
+        retorno.addObject("nome", nome);
+        retorno.addObject("email", email);
+        retorno.addObject("senha", senha);
+        retorno.addObject("modulo", modulo);
+        retorno.addObject("preferencia", preferencia);
+
+        return retorno;
+    }
 }
